6aa580bacc9736165387f44ef1918d1aa32f10a9
Mirzan <mirzansiva2008@gmail.com>
2026-09-24 16:34:51 +0530

    Create printdemo.java

---
+++ printdemo.java
@@ -0,0 +1,24 @@
+class Print{
+	public static void main(String args[]){
+		system.out.println("MY name is Seelan"); //Using println 
+		system.out.println("My father name is Yogan");
+		system.out.println("My age is 24");
+		
+		
+		system.out.println();
+		
+		
+		system.out.print("My name is Seelan"); // Using print
+		system.out.print("My father name is Yogan");
+		system.out.print("My age is 24");
+		
+		
+		system.out.println();
+		
+		
+		// Using print with \n
+		system.out.print("My name is Seelan\n");
+		system.out.print("my father name is Yogan\n");
+		system.out.system("my age is 24\n");
+	}
+}
